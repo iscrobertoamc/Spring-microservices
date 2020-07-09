7a9d2dea2d64f223377735df9e20794b8fe22660
Roberto Morales <roberto.morales@fonyou.com>
2020-07-09 18:31:32 -0500

    back up 101

--- currency-convertion-service/src/main/java/com/training/microservices/currencyconvertionservice/CurrencyConvertionServiceApplication.java
+++ currency-convertion-service/src/main/java/com/training/microservices/currencyconvertionservice/CurrencyConvertionServiceApplication.java
@@ -2,10 +2,12 @@ package com.training.microservices.currencyconvertionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.training.microservices.currencyconvertionservice")
+@EnableDiscoveryClient
 public class CurrencyConvertionServiceApplication {
 
 	public static void main(String[] args) {
