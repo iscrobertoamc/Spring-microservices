784881928292e3c3fa86c7d9f795a4f7b467de3d
Roberto Morales <roberto.morales@fonyou.com>
2020-07-16 13:09:04 -0500

    last step

--- limit-service/src/main/java/com/training/microservies/limitservice/controller/LimistConfigurationController.java
+++ limit-service/src/main/java/com/training/microservies/limitservice/controller/LimistConfigurationController.java
@@ -4,17 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.training.microservies.limitservice.Configuration;
 import com.training.microservies.limitservice.model.LimitConfiguration;
 
 @RestController
 public class LimistConfigurationController {
-	
+
 	@Autowired
 	private Configuration configuration;
-	
+
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigirations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetriveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not avalable");
+	}
+
+	public LimitConfiguration fallbackRetriveConfiguration(){
+		return new LimitConfiguration(333, 3);
 	}
 }
